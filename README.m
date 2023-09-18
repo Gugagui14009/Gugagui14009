3d875278becb90c01544b8c3ba1c87438d09a945
Gugagui14009 <145357870+Gugagui14009@users.noreply.github.com>
2023-09-18 15:08:16 -0300

    Update README.m

--- README.m
+++ README.m
@@ -1,6 +1,6 @@
 - 👋 Olá, sou @Gugagui14009
-- 📫 Como entrar em contato comigo ...(44)997215065
+- 📫 Como entrar em contato comigo ..._guuh.hr
 MESSI E MELHOR QUE CR7😈🙈
 𝑽𝒊𝒗𝒆𝒏𝒅𝒐 𝒆 𝒂𝒑𝒓𝒆𝒏𝒅𝒆𝒏𝒐🍃
-![](https://img.ibxk.com.br/2018/11/21/elon-musk-21155134424155.gif)
+![](https://gifdb.com/images/high/messi-snap-flying-kiss-12gxsfu8z5v9b1a0.gif)
 

bef4f463e878b9ab04b19146d0a097444a89fafd
Gugagui14009 <145357870+Gugagui14009@users.noreply.github.com>
2023-09-18 15:13:12 -0300

    Update README.m
    ![ ] (https://media0.giphy.com/media/3nrOR6jPIbqV2/200.gif)

--- README.m
+++ README.m
@@ -1,7 +1,7 @@
 - 👋 Olá, sou @Gugagui14009
 - 📫 Como entrar em contato comigo ..._guuh.hr
 MESSI E MELHOR QUE CR7😈🙈
-𝑽𝒊𝒗𝒆𝒏𝒅𝒐 𝒆 𝒂𝒑𝒓𝒆𝒏𝒅𝒆𝒏𝒐🍃
+𝑽𝒊𝒗𝒆𝒏𝒅𝒐 𝒆 𝒂𝒑𝒓𝒆𝒏𝒅𝒆𝒏d𝒐🍃
 
-![](https://media0.giphy.com/media/3nrOR6jPIbqV2/200.gif)
+! [](https://media0.giphy.com/media/3nrOR6jPIbqV2/200.gif
 
